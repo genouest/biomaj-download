78b8ee668f33641773a40da210c13a0304841b6e
Olivier Sallou <olivier.sallou@irisa.fr>
2016-09-22 17:58:22 +0200

    fix PEP8

--- biomaj_download/biomaj_download_web.py
+++ biomaj_download/biomaj_download_web.py
@@ -5,15 +5,11 @@ Manage sessions and metrics
 
 import ssl
 import os
-import threading
-import logging
 
 import yaml
 from flask import Flask
 from flask import jsonify
-from flask import g
 from flask import request
-from flask import Response
 from prometheus_client import Counter
 from prometheus_client import Gauge
 from prometheus_client.exposition import generate_latest
@@ -26,8 +22,8 @@ app = Flask(__name__)
 
 download_metric = Counter("biomaj_download_total", "Bank total download.", ['bank'])
 download_error_metric = Counter("biomaj_download_errors", "Bank total download errors.", ['bank'])
-download_size_metric =  Gauge("biomaj_download_file_size", "Bank download file size in bytes.", ['bank'])
-download_time_metric =  Gauge("biomaj_download_file_time", "Bank download file time in seconds.", ['bank'])
+download_size_metric = Gauge("biomaj_download_file_size", "Bank download file size in bytes.", ['bank'])
+download_time_metric = Gauge("biomaj_download_file_time", "Bank download file time in seconds.", ['bank'])
 
 config_file = 'config.yml'
 if 'BIOMAJ_CONFIG' in os.environ:
@@ -48,28 +44,15 @@ def start_server(config):
         consul_agent = consul.Consult(host=config['consul']['host'])
         consul_agent.agent.service.register('biomaj_download', service_id=config['consul']['id'], port=config['web']['port'], tags=['biomaj'])
         check = consul.Check.http(url=config['web']['local_endpoint'], interval=20)
-        consul_agent.agent.check.register(name + '_check', check=check, service_id=config['consul']['id'])
-
+        consul_agent.agent.check.register(config['consul']['id'] + '_check', check=check, service_id=config['consul']['id'])
 
     app.run(host='0.0.0.0', port=config['web']['port'], ssl_context=context, threaded=True, debug=config['web']['debug'])
 
 
-def shutdown_server():
-    #download.channel.cancel()
-    download.channel.stop_consuming('biomajdownload')
-    func = request.environ.get('werkzeug.server.shutdown')
-    if func is None:
-        raise RuntimeError('Not running with the Werkzeug Server')
-    func()
-
-@app.route('/api/download/shutdown', methods=['POST'])
-def shutdown():
-    shutdown_server()
-    return 'Server shutting down...'
-
 @app.route('/api/download/metrics', methods=['GET'])
 def metrics():
     return generate_latest()
+
 
 @app.route('/api/download/metrics', methods=['POST'])
 def add_metrics():
@@ -86,6 +69,7 @@ def add_metrics():
             download_time_metric.labels(downloaded_file['bank']).set(downloaded_file['download_time'])
     return jsonify({'msg': 'OK'})
 
+
 @app.route('/api/download/status/list/<bank>/<session>')
 def list_status(bank, session):
     '''
@@ -99,6 +83,7 @@ def list_status(bank, session):
     status = dserv.list_status(biomaj_file_info)
     return jsonify({'status': status})
 
+
 @app.route('/api/download/status/download/<bank>/<session>')
 def download_status(bank, session):
     '''
@@ -111,6 +96,7 @@ def download_status(bank, session):
     biomaj_file_info.local_dir = '/tmp'
     (progress, errors) = dserv.download_status(biomaj_file_info)
     return jsonify({'progress': progress, 'errors': errors})
+
 
 @app.route('/api/download/error/download/<bank>/<session>')
 def download_error(bank, session):
@@ -138,13 +124,14 @@ def list_result(bank, session):
     biomaj_file_info.local_dir = '/tmp'
     list_elts = dserv.list_result(biomaj_file_info, protobuf_decode=False)
     return jsonify({'files': list_elts})
-    #return Response(list_elts, mimetype="application/x-protobuf")
+
 
 @app.route('/api/download/session/<bank>', methods=['POST'])
 def create_session(bank):
     dserv = DownloadService(config_file, rabbitmq=False)
     session = dserv._create_session(bank)
     return jsonify({'session': session})
+
 
 @app.route('/api/download/session/<bank>/<session>', methods=['DELETE'])
 def clean_session(bank, session):
