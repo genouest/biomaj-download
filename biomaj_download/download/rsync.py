df27749dac31d8ec3b58ee9d27ab28ca41212030
chloe riou <chloe.riou@inria.fr>
2016-11-22 10:10:45 +0100

    compatibility flake8

--- biomaj_download/download/rsync.py
+++ biomaj_download/download/rsync.py
@@ -63,7 +63,7 @@ class RSYNCDownload(DownloadInterface):
             logging.error("RsyncError:" + str(e))
         if err_code != 0:
             logging.error('Error while listing ' + str(err_code))
-            return(rfiles, rdirs)        
+            return(rfiles, rdirs)
         list_rsync = str(list_rsync.decode('utf-8'))
         lines = list_rsync.rstrip().split("\n")
         for line in lines:
@@ -164,14 +164,12 @@ class RSYNCDownload(DownloadInterface):
         return(error)
 
     def test_stderr_rsync_error(self, stderr):
-        #stderr = str(stderr, encoding='utf8')
         stderr = str(stderr.decode('utf-8'))
         if "rsync error" in str(stderr):
             reason = stderr.split(str(self.protocol) + " error:")[1].split("\n")[0]
             raise ExceptionRsync(reason)
 
     def test_stderr_rsync_message(self, stderr):
-        #stderr = str(stderr, encoding='utf8')
         stderr = str(stderr.decode('utf-8'))
         if "rsync:" in str(stderr):
             reason = stderr.split(str(self.protocol) + ":")[1].split("\n")[0]
