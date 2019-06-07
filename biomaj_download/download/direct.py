2fe412b5fe3db3c765790ae8b794ed1611899cd4
lburlot <lburlot@genoscope.cns.fr>
2019-06-07 15:25:29 +0100

    Delete duplicate header_function (it already exists in the FTPDownload class).

--- biomaj_download/download/direct.py
+++ biomaj_download/download/direct.py
@@ -101,34 +101,6 @@ class DirectHttpDownload(DirectFTPDownload):
             # TODO: raise exception ?
         return super(DirectHttpDownload, self).download(local_dir, keep_dirs)
 
-    def header_function(self, header_line):
-        # HTTP standard specifies that headers are encoded in iso-8859-1.
-        # On Python 2, decoding step can be skipped.
-        # On Python 3, decoding step is required.
-        header_line = header_line.decode('iso-8859-1')
-
-        # Header lines include the first status line (HTTP/1.x ...).
-        # We are going to ignore all lines that don't have a colon in them.
-        # This will botch headers that are split on multiple lines...
-        if ':' not in header_line:
-            return
-
-        # Break the header line into header name and value.
-        name, value = header_line.split(':', 1)
-
-        # Remove whitespace that may be present.
-        # Header lines include the trailing newline, and there may be whitespace
-        # around the colon.
-        name = name.strip()
-        value = value.strip()
-
-        # Header names are case insensitive.
-        # Lowercase name here.
-        name = name.lower()
-
-        # Now we can actually record the header name and value.
-        self.headers[name] = value
-
     def list(self, directory=''):
         '''
         Try to get file headers to get last_modification and size
