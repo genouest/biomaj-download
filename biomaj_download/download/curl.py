f950b20b22442ab9ba288613cdf004c1c736d850
Mathieu Dubois <mdubois@genoscope.cns.fr>
2019-10-14 13:25:56 +0200

    Reorganize file.

--- biomaj_download/download/curl.py
+++ biomaj_download/download/curl.py
@@ -198,6 +198,34 @@ class CurlDownload(DownloadInterface):
         # Header function
         self.crl.setopt(pycurl.HEADERFUNCTION, self.header_function)
 
+    def header_function(self, header_line):
+        # HTTP standard specifies that headers are encoded in iso-8859-1.
+        # On Python 2, decoding step can be skipped.
+        # On Python 3, decoding step is required.
+        header_line = header_line.decode('iso-8859-1')
+
+        # Header lines include the first status line (HTTP/1.x ...).
+        # We are going to ignore all lines that don't have a colon in them.
+        # This will botch headers that are split on multiple lines...
+        if ':' not in header_line:
+            return
+
+        # Break the header line into header name and value.
+        name, value = header_line.split(':', 1)
+
+        # Remove whitespace that may be present.
+        # Header lines include the trailing newline, and there may be whitespace
+        # around the colon.
+        name = name.strip()
+        value = value.strip()
+
+        # Header names are case insensitive.
+        # Lowercase name here.
+        name = name.lower()
+
+        # Now we can actually record the header name and value.
+        self.headers[name] = value
+
     def set_options(self, protocol_options):
         super(CurlDownload, self).set_options(protocol_options)
         if "ssl_verifyhost" in protocol_options:
@@ -280,34 +308,6 @@ class CurlDownload(DownloadInterface):
             nbtry += 1
 
         return error
-
-    def header_function(self, header_line):
-        # HTTP standard specifies that headers are encoded in iso-8859-1.
-        # On Python 2, decoding step can be skipped.
-        # On Python 3, decoding step is required.
-        header_line = header_line.decode('iso-8859-1')
-
-        # Header lines include the first status line (HTTP/1.x ...).
-        # We are going to ignore all lines that don't have a colon in them.
-        # This will botch headers that are split on multiple lines...
-        if ':' not in header_line:
-            return
-
-        # Break the header line into header name and value.
-        name, value = header_line.split(':', 1)
-
-        # Remove whitespace that may be present.
-        # Header lines include the trailing newline, and there may be whitespace
-        # around the colon.
-        name = name.strip()
-        value = value.strip()
-
-        # Header names are case insensitive.
-        # Lowercase name here.
-        name = name.lower()
-
-        # Now we can actually record the header name and value.
-        self.headers[name] = value
 
     def list(self, directory=''):
         '''
