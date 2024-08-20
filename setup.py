061e5623931dd9cf6b684321b96de780abe52c3b
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2024-08-20 15:05:56 +0200

    ci/build upgrade

--- setup.py
+++ setup.py
@@ -22,10 +22,10 @@ config = {
     'description': 'BioMAJ download service',
     'long_description': README + '\n\n' + CHANGES,
     'long_description_content_type': 'text/markdown',
-    'author': 'Olivier Sallou',
-    'url': 'http://biomaj.genouest.org',
-    'download_url': 'http://biomaj.genouest.org',
-    'author_email': 'olivier.sallou@irisa.fr',
+    'author': 'GenOuest platform',
+    'url': 'https://biomaj.genouest.org',
+    'download_url': 'https://biomaj.genouest.org',
+    'author_email': 'support@genouest.org',
     'version': '3.2.11',
     'classifiers': [
         # How mature is this project? Common values are
